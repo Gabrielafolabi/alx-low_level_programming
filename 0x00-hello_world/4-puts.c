774a71d2309fb197db20f9332d09f69f1dce4fc7
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-07 12:29:39 -0700

    cor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main = Entry point
+ *
+ * Return value always (success)
+ */
+
+
+
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 
 
 }
