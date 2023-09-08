8f5718827329f2f0450cd5c632c1e9555ccdb880
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-08 04:30:54 -0700

    cor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,14 +6,12 @@
  *
  * Return: Always 1 (Success)
  */
-int main(void) 
+int main(void)
 {
-    const char *message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n";
-    ssize_t len = strlen(message);
 
-    if (write(STDERR_FILENO, message, len) != len) 
-    {
-        return 1;
-    }
-    return 1;
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-  19\n";
+	write(2, quote, 59);
+	return (1);
+
 }
+
