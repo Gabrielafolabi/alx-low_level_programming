6ab8ecd0273c08c413f4848dfde415e75f862513
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-08 04:47:28 -0700

    cor

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,15 +3,11 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-
-	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-  19\n";
+char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-  19\n";
 	write(2, quote, 59);
 	return (1);
-
 }
-
