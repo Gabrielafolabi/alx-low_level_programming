29b09335a63b780d00f3c73c59c3c5d70ec6a93d
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-08 20:27:34 -0700

    cor

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,7 @@
 
 /**
  * main - Prints the last digit of a randomly generated number.
+ * 	follow by a new line
  * Return: Always 0.
  */
 int main(void)
@@ -26,3 +27,4 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
+}
