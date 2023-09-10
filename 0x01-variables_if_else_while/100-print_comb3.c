d05841021f2c62265c44a65f86bf932af3e28582
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-10 12:31:20 -0700

    Dcor

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,18 +10,18 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1=0, num1<=9, num2++);
+	for (num1 = 0; num1 < 10; num2++)
 	{
-		for (num2=0, num2<=9, num2++);
+		for (num2 = 0; num2 < 10; num2++)
 		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
+
+			putchar(',');
+			putchar(' ');
 		
 		}	 
 	
-
-			putchar(",");
-			putchar(" ");
 
 	}
 
