67c94ae2f98cf7bb655ce04cdee73d9dd03876a2
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-13 07:17:11 -0700

    cor

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * jack_bauer - Write a function that prints every minute of the day of Jack Bauer, 
- * 	starting from 00:00 to 23:59.
- *   
+ * jack_bauer -Function that prints every minute of the day of Jack Bauer.
+ *	starting from 00:00 to 23:59.
+ *
  */
 
 void jack_bauer(void)
