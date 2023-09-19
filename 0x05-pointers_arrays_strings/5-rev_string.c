6f8adae3578fd1e396c709d6f0e1d017ed59b7d4
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-19 13:45:49 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be modified
+  *
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	int i, b, c;
+	char *a, rev;
+
+	a = s;
+
+	while (s[b] != '\0')
+	{
+		b++;
+	}
+
+	for (c = 1; c < b; c++)
+	{
+		a++;
+	}
+
+	for (i = 0; i < (b / 2); i++)
+	{
+		rev = s[i];
+		s[i] = *a;
+		*a = rev;
+		a--;
+	}
+}
