6288a9e89ab8f81ef70b7d19195497e4c9cf4717
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-19 13:21:00 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+  * print_rev - Prints a string in reverse
+  * @s: The string to print
+  *
+  * Return: void
+  */
+
+
+void revstr(char *s) 
+{ 
+
+	int i, len, temp; 
+	len = strlen(s);  
+
+
+ 	for (i = 0; i < len/2; i++) 
+ 	{ 
+  		temp = s[i]; 
+		s[i] = s[len - i - 1]; 
+		s[len - i - 1] = temp; 
+	} 
+ } 
