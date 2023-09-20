87f166931fad7472623f1d2fc5bf73d533b70726
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-20 16:23:23 -0700

    2--strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _strncpy - Copy a string
+  * @dest: The destination value
+  * @src: The source value
+  * @n: The copy limit
+  *
+  * Return: char value
+  */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int len_string;
+
+	for (len_string = 0; len_string < n && src[len_string] != '\0')
+	{
+		dest[len_string] = src[len_string];
+		len_string++;
+	}
+
+	for (; len_string < n ; len_string++)
+	
+	dest[len_string] = '\0';
+	return (dest);
+}
