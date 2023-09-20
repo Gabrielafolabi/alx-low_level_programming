2ff9c1491d04cd07573386e8863c7017cc127235
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-20 16:25:32 -0700

    2--strncat

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,7 @@ char *_strncpy(char *dest, char *src, int n)
 
 	int len_string;
 
-	for (len_string = 0; len_string < n && src[len_string] != '\0')
+	for (len_string = 0; len_string < n && src[len_string] != '\0';)
 	{
 		dest[len_string] = src[len_string];
 		len_string++;
