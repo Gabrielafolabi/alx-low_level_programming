b00aaafc3946909bcc32f07a6e4ce67703988549
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-21 16:01:17 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * string_toupper - function that change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int len;
+
+	len = 0;
+
+	while (n[len] != '\0')
+	{
+		if (n[len] >= 'a' && n[len] <= 'z')
+		{
+			n[len] = n[len] - 32;
+		}
+		i++;
+	}
+	return (n);
+}
