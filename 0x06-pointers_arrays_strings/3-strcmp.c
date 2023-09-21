7d50c2ff2e421134737e974cf1eb6e9e57878b19
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-21 07:29:20 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,23 +4,22 @@
 /**
  * _strcmp - function that compares two strings
  *
- * @str1 - pointer to the first string
- * @str2 - pointer to the second string
+ * @s1 - pointer to the first string
+ * @s2 - pointer to the second string
  * Return: 0 if string is equal
  */
 
 int _strcmp(char *s1, char *s2)
 {
 
-	int len; 
+	int len;
 	int value;
+
 	len = 0;
 
 	while (s1[len] == s2[len] && s1[len] != '\0')
 		len++;
 
 	value = s1[len] - s2[len];
-	return (value);	
-
-
+	return (value);
 }
