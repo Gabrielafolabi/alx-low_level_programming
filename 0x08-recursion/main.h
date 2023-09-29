defea3bbc9e8dafc4034e113ce7b442bf618d3d1
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-09-29 14:53:23 -0700

    100-is_palindrome.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,8 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int _putchar(char c);
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
 #endif
