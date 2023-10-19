2709d22763b6e89d8f6c58341cb53cfe25b693ca
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-19 13:20:53 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+
+
+/**
+ * print_tor - Function that Prints a string before the
+ *	executing main function.
+ */
+void print_tor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
