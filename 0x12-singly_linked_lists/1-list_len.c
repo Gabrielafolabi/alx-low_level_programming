599693e0500b4137ea18131d0e1a825b34cdd826
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-19 10:03:44 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,9 +3,9 @@
 /**
  * list_len - Function that finds the number of elements in
  *            a linked list_t list.
- * @h: The linked list_t list.
+ * @i: Linked list_t list.
  *
- * Return: The number of elements in h.
+ * Return: The number of elements in i.
  */
 size_t list_len(const list_t *i)
 {
