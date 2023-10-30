15a727507b70f5cc8559d59a1b919ffd8140e16c
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-30 12:49:56 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * read_textfile - function that reads a text file and prints 
+  * @filename: The source file
+  * @letters: Number of letters
+  *
+  * Return: Success always
+  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int f, rd;
+	char *buff = malloc(sizeof(char *) * letters);
+
+	if (!buff)
+	{	
+		return (0);
+	}
+	if (!filename)
+	{	
+		return (0);
+	}
+	f = open(filename, O_RDONLY, 0600);
+	if (f == -1)
+	{	
+		return (0);
+	}
+	rd = read(f, buff, letters);
+	write(STDOUT_FILENO, buff, rd);
+
+	free(buff);
+	close(f);
+	return (rd);
+}
