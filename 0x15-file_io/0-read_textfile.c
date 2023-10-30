4c9c5d1ca33d87a6576a9c1d7854f9a9a5ca6710
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-10-30 13:18:30 -0700

    1-create_file.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,9 +3,9 @@
 /**
   * read_textfile - function that reads a text file and prints 
   * @filename: The source file
-  * @letters: Number of letters
+  * @letters: Number of letter
   *
-  * Return: Success always
+  * Return: Success alway
   */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -13,16 +13,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buff = malloc(sizeof(char *) * letters);
 
 	if (!buff)
-	{	
+	{
 		return (0);
 	}
 	if (!filename)
-	{	
+	{
 		return (0);
 	}
 	f = open(filename, O_RDONLY, 0600);
 	if (f == -1)
-	{	
+	{
 		return (0);
 	}
 	rd = read(f, buff, letters);
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+  * create_file - function that creates a file
+  * @filename: 
+  * @text_content: 
+  *
+  * Return: Always success
+  */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+
+	if (!filename)
+	{	
+		return (-1);
+	}
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f == -1)
+	{	
+		return (-1);
+	}
+	if (text_content)
+	{	
+		write(f, text_content, _strlen(text_content));
+	}
+	close(f);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: Always success
+  */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+		count++;
+
+	return (count);
+}
