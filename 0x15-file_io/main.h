1dc69912fc8ddac1e756d862edd0420d15826cbb
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2023-11-02 04:13:14 -0700

    elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,13 +9,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *s);
 void copy_file(const char *src, const char *dest);
+
 
 
 #endif 
