f7d162bcdbc877785d067df16e679a8e07e1179e
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2024-02-22 08:14:01 -0800

    updated

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,13 @@
+#include "MAIN_HEAD"
+
+/**
+ * Main - Entry
+ *
+ * Return: Success Always
+ */
+
+int main(void);
+{
+	prnt_School();
+	return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_HEAD
+#define MAIN_HEAD
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void prnt_School(void);
+#endif
