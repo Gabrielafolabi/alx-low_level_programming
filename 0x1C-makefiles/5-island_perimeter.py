2abfc2f556dc145ae6d3434978e8e48ab21db84a
Gabriel Afolabi <olaoluwagabriel73@gmail.com>
2024-02-22 08:50:35 -0800

    updated

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defining 5-island_perimeter module"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of an island""
+    a = 0
+    height = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for x, y in range(grid):
+        for b, n in range(y):
+            if n == 1:
+                if x == 0 or grid[x- 1][b] != 1:
+                    c += 1
+                if b == 0 or grid[x][b - 1] != 1:
+                    c += 1
+                if b == width or grid[x][b + 1] != 1:
+                    c += 1
+                if x == length or grid[x + 1][b] != 1:
+                    a += 1
+    return a
